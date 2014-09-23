bcab5a4419f0a38929cd76d01355ee8f6648e08c
Danial <Danial@DanialPC>
2014-09-23 18:49:10 +0200

    First commit on local repository after setting up a test project

---
+++ src/lingpipetestproject/AnotherTestClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lingpipetestproject;
+
+/**
+ *
+ * @author Danial
+ */
+public class AnotherTestClass {
+    
+    public void TestFunction()
+            
+    {
+        System.out.println("This is the print out from test function in the test class");
+    }
+    
+}
